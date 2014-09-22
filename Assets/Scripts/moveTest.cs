4b9255dc6806109cc04c1b89a88fa01ccda5616a
nagamasa0122 <nagao.masayuki@gmail.com>
2014-09-23 00:25:25 +0900

    test
    testコミット

---
+++ Assets/Scripts/moveTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveTest : MonoBehaviour {
+
+	public GameObject prefab;
+
+	private GameObject inst_prefab;
+
+	// Use this for initialization
+	void Start () {
+		if( prefab != null )
+			inst_prefab = GameObject.Instantiate(prefab) as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( inst_prefab != null ){
+			inst_prefab.transform.position = new Vector3(0, 5, 0);
+		}
+	}
+}
